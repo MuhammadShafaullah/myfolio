b6d36cf5c1d688407f010bcfd52115e1010e9c80
Muhammad Shafaullah <72027876+MuhammadShafaullah@users.noreply.github.com>
2023-01-15 23:06:04 +0500

    Update settings.py

--- myfolio/settings.py
+++ myfolio/settings.py
@@ -66,10 +66,10 @@ WSGI_APPLICATION = 'myfolio.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    #'default': {
-    #    'ENGINE': 'django.db.backends.sqlite3',
-    #    'NAME': BASE_DIR / 'db.sqlite3',
-    #}
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 
